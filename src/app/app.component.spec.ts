8ec79a931fa5f1c24503d26eff9fe81ebe265838
MSI\smyly <smylydon@yahoo.com>
2023-08-24 10:11:30 -0700

    Added header and css.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -15,12 +14,21 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome blog');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Redux Blog');
   });
 
   it(`should have as title 'blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('blog');
+  });
+
+  it('should render 2 menu items', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const selected = compiled.querySelectorAll('a');
+    expect(selected[0]?.textContent).toContain('Home');
+    expect(selected[1]?.textContent).toContain('Post');
   });
 });
