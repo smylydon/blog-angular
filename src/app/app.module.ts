6570e3476f554a0d8a5bd236717ee2a566375b26
MSI\smyly <smylydon@yahoo.com>
2023-08-24 19:59:33 -0700

    Forgot I deleted the welcome component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RoutingModule } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RoutingModule,
